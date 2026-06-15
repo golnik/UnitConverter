05f078ba6b4bc945f408b2eb359f0f15c36c9315
Nikolay Golubev <nik.v.golubev@gmail.com>
2026-06-14 17:27:11 -0700

    more accurate NIST values of constants are implemented

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,8 @@
+var c = 299792458;          //m/s
+var h = 4.135667696e-15;    //eV/Hz
+var au2eV = 27.211386245981;
+
 var calculator = function(energy){
-    var c = 2.9979e8;
-    var h = 4.135667516e-15;
     var newValues = [
         0,
         0,
@@ -16,16 +18,14 @@ var calculator = function(energy){
     newValues[1] = (h*c*1e9)/energy;  //nm
     newValues[2] = energy/(h*c*1e2);  //cm-1
     newValues[3] = 1e3*(h*1e12)/energy;//fs
-    newValues[4] = energy*0.0367492929;//Hartree
+    newValues[4] = energy/au2eV;//Hartree
     newValues[5] = h*c*1e6/energy;    // um
     newValues[6] = energy/(h*1e12);   // THz
-    newValues[7] = 170.017573254/energy;//aut
+    newValues[7] = 2*Math.PI*au2eV/energy;//aut
     return newValues
 };
 
 var converter = function(doc,sender){
-    var c = 2.9979e8;
-    var h = 4.135667516e-15;
     var elements = [
         doc.getElementById("eV"),
         doc.getElementById("nm"),
@@ -52,7 +52,7 @@ var converter = function(doc,sender){
             toSkip = doc.getElementById("cm");
             break;
         case 'hartree':
-            energy = parseFloat(27.211386245981*doc.getElementById("hartree").value);
+            energy = parseFloat(au2eV*doc.getElementById("hartree").value);
             toSkip = doc.getElementById("hartree");
             break;
         case 'um':
@@ -68,7 +68,7 @@ var converter = function(doc,sender){
             toSkip = doc.getElementById("fs");
             break;
         case 'aut':
-            energy = 170.017573254/parseFloat(doc.getElementById("aut").value);
+            energy = 2*Math.PI*au2eV/parseFloat(doc.getElementById("aut").value);
             toSkip = doc.getElementById("aut");
             break;
         default:
